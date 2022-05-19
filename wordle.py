cbc68ad655696bae214a37cc6d1d776c12d1a08a
iniadewumi <72786828+iniadewumi@users.noreply.github.com>
2022-05-19 13:57:34 -0500

    chromedriver and requirements

--- wordle.py
+++ wordle.py
@@ -1,10 +1,13 @@
 
 import itertools
 from selenium import webdriver
-import pyautogui as pag
 import json, pathlib
+from time import sleep
+# import pyautogui as pag
 
-USER_DATA = pathlib.Path().resolve() / 'Rick'
+HOME = pathlib.Path().resolve() 
+cdr = HOME.parent
+USER_DATA = HOME / 'Data'
 USER_DATA.mkdir(exist_ok=True, parents=True)
 
 class ChromeNotFoundException(Exception):
@@ -26,7 +29,8 @@ class Wordle:
             if len(self.correct.keys())>=4 and len(self.potential)<2:
                 print(f"Word found: {self.potential.pop()}")
                 break
-            input("\nPress Enter after playing: ")
+            # input("\nPress Enter after playing: ")
+            sleep(10)
             board = browser.execute_script("return document.getElementsByTagName('game-app')[0].$board")
             
             self.correct  = {}
@@ -50,7 +54,10 @@ class Wordle:
             self.not_in = list(set(self.not_in + [x for x in letters if x not in list(self.correct.keys())+self.present]))
 
 
-        self.potential = {word for word in to_check if ({i: v for i, v in self.correct.items() if word[v] == i} == self.correct and not any(x for x in self.not_in if x in word) and sorted([x for x in self.present if x in word]) == sorted(self.present) and not [let for let, j in self.wrong_pos.items() if word[j] == let])}
+        self.potential = {word for word in to_check if ({i: v for i, v in self.correct.items() 
+                                                         if word[v] == i} == self.correct and 
+                                                        not any(x for x in self.not_in if x in word) and 
+                                                        sorted([x for x in self.present if x in word]) == sorted(self.present) and not [let for let, j in self.wrong_pos.items() if word[j] == let])}
         self.get_letter_freq()
     def get_letter_freq(self):
         self.letter_freq = {}
@@ -69,9 +76,13 @@ class Wordle:
                 self.ranked_pot[word] = self.ranked_pot.get(word, 0) + v
         self.ranked_pot = dict(sorted(self.ranked_pot.items(), key=lambda x: x[1], reverse=True))
         print(list(self.ranked_pot)[:50][::-1])
-        print(self.letter_freq)
+        print(f"Letter Frequency:\n{self.letter_freq}")
+        print("\n\nTop 5 words (I always start with ORATE):")
+        print('\n'.join(list(self.ranked_pot)[:5]))
+        print("\nTo eliminate as many potential words as possible, use words containing the following letters: ")
         
-
+        not_in_poss = [x for x in self.letter_freq.keys() if x not in self.present]
+        print(" ".join(not_in_poss[:5]))
 
 import subprocess, datetime
 date = datetime.datetime.now()
@@ -80,30 +91,30 @@ date +=  datetime.timedelta(days=1)
 
 # subprocess.check_output(["date", date.strftime("%m-%d-%y")], shell=True)
 
-try:
-    # PYTHON Example
-    winds = next(x for x in pag.getAllTitles() if "google chrome" in x.lower() and "wordle" in x.lower())
+# try:
+#     # PYTHON Example
+#     winds = next(x for x in pag.getAllTitles() if "google chrome" in x.lower() and "wordle" in x.lower())
 
 
-    if not winds:
-        raise ChromeNotFoundException("Chrome not found!")
-    print("Trying to access old Chrome Browser at Port: 9222")
+#     if not winds:
+#         raise ChromeNotFoundException("Chrome not found!")
+#     print("Trying to access old Chrome Browser at Port: 9222")
 
-    from selenium.webdriver.chrome.options import Options
-    chrome_options = Options()
-    chrome_options.add_argument(f"user-data-dir={USER_DATA}")
-    chrome_options.add_argument('log-level=3')
-    chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-    browser = webdriver.Chrome(options=chrome_options)
-    print("Successfully accessed old Chrome Browser at Port: 9222")
+#     from selenium.webdriver.chrome.options import Options
+#     chrome_options = Options()
+#     chrome_options.add_argument(f"user-data-dir={USER_DATA}")
+#     chrome_options.add_argument('log-level=3')
+#     chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+#     browser = webdriver.Chrome(options=chrome_options, executable_path=cdr/"chromedriver.exe")
+#     print("Successfully accessed old Chrome Browser at Port: 9222")
 
-except Exception:
-    print("Failed to access old Chrome Browser at Port: 9222\nCreating new Chrome Browser")
-    chrome_options1 = webdriver.ChromeOptions()
-    chrome_options1.add_argument(f"user-data-dir={USER_DATA}")
-    chrome_options1.add_argument('--remote-debugging-port=9222')
-    chrome_options1.add_argument('log-level=3')
-    browser = webdriver.Chrome(options=chrome_options1)
+# except Exception:
+#     print("Failed to access old Chrome Browser at Port: 9222\nCreating new Chrome Browser")
+chrome_options1 = webdriver.ChromeOptions()
+chrome_options1.add_argument(f"user-data-dir={USER_DATA}")
+chrome_options1.add_argument('--remote-debugging-port=9222')
+chrome_options1.add_argument('log-level=3')
+browser = webdriver.Chrome(options=chrome_options1)
 
 
 wordle = Wordle()
